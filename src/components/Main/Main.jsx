f35f0250274d7a3f3c6dff9c755d675d8b1c9c81
raihan <104217620+abiyyuraihan@users.noreply.github.com>
2024-03-10 03:11:49 +0700

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,94 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+const Main = () => {
+
+  const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context)
+
+  return (
+    <div className='main'>
+
+      <div className="nav">
+        <p>RaihanGPT</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+
+
+
+      <div className="main-container">
+        {!showResult
+          ? <>
+            <div className="greet">
+              <p><span>Hai Manusia</span></p>
+              <p>Ada yang bisa ane bantu ga ni?</p>
+            </div>
+
+            <div className="cards">
+
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Improve the readability of the following code</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+
+            </div>
+          </>
+          : <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+
+            <div className="result-data">
+              <img src={assets.gemini_icon} alt="" />
+              {loading
+                ? <div className='loader'>
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+                : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              }
+
+            </div>
+          </div>
+        }
+
+
+
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Tulis Pertanyaan Ente dimari' />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input?<img onClick={() => onSent()} src={assets.send_icon} alt="" />:null}
+            </div>
+
+          </div>
+          <p className="bottom-info">
+            Jawaban Ane Mungkin kagak 100% bener, jadi jangan langsung percaya, mending ente cek lagi dah...
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Main
